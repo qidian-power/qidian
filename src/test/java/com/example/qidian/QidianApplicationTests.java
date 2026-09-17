78b2013b5624ae33133f528e507baddedb2be37e
qidian <1835551573@qq.com>
2026-09-17 16:47:12 +0800

    springboot项目初始状态

---
+++ src/test/java/com/example/qidian/QidianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qidian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QidianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
